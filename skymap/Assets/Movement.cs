e9606086759387609d37eeee48f036a5242c213b
Solomon F Mercurio <solomon12@me.com>
2015-12-01 20:01:32 -0500

    no message

--- skymap/Assets/Movement.cs
+++ skymap/Assets/Movement.cs
@@ -17,26 +17,14 @@ public class Movement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-
-        //gameObject.transform.position = new Vector3(hand.transform.position.x * 15, hand.transform.position.y * 12, distance);
-
-        //gameObject.transform.position = new Vector3(hand.transform.position.x, hand.transform.position.y, distance);
+        gameObject.transform.position = new Vector3(hand.transform.position.x * 15, hand.transform.position.y * 12, distance);
 
 
-
-
-        //gameObject.transform.position = Camera.main.WorldToScreenPoint(new Vector3(hand.transform.position.x * 15, hand.transform.position.y * 12, distance));
-
-        positions = Camera.main.WorldToScreenPoint(new Vector3(hand.transform.position.x, hand.transform.position.y, distance));
-
-        gameObject.transform.position = new Vector3(positions.x * .05f, positions.y * .05f, distance);
-
-
-        ////Working on this below to change camera movements
+        //Working on this below to change camera movements 
         //if (hand.transform.position.x > .5f)
         //{
-        //    gameObject.transform.position = new Vector3(hand.transform.position.x * 15 * Mathf.Cos(0.349066f), hand.transform.position.y * 12, distance);
-        //    //gameObject.transform.Rotate(Vector3.up, 20.0f * Time.deltaTime);
+        //    gameObject.transform.position = new Vector3(hand.transform.position.x * 15, hand.transform.position.y * 12, distance);
+        //    gameObject.transform.Rotate(Vector3.up, 20.0f * Time.deltaTime);
         //}
 
         //if (hand.transform.position.x < -0.3f)
