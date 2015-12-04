64066f1c91be2023cb297506f55ed2ef9a21de60
Sara Artese <saa2457@rit.edu>
2015-12-04 02:44:23 -0500

    Added in cursors and collision detection boxes.  Hid most button functions until we get full functionality of the buttons and collision.

--- skymap/Assets/Script/MenuScript.cs
+++ skymap/Assets/Script/MenuScript.cs
@@ -8,23 +8,22 @@ public class MenuScript : MonoBehaviour {
 	public Canvas creditMenu;
     public Button startText;
     public Button exitText;
+    public GameObject colYes;
     public GameObject rightHand;
     public GameObject leftHand;
-    public GameObject enterObj;
     float timeLeft = 5.0f;
    
-    //Testing
 	// Use this for initialization
    
 	void Start () 
     {
         quitMenu = quitMenu.GetComponent<Canvas>();
-		creditMenu = creditMenu.GetComponent<Canvas>();
-
         startText = startText.GetComponent<Button>();
         exitText = exitText.GetComponent<Button>();
-
         quitMenu.enabled = false;
+       // GameObject.Find("colYes").SetActive(false);
+
+		creditMenu = creditMenu.GetComponent<Canvas>();
 		creditMenu.enabled = false;
 
 	}
@@ -74,14 +73,13 @@ public class MenuScript : MonoBehaviour {
 
     void OnCollisionEnter(Collision col)
     {
-       
+
         if (col.gameObject.name == "enterObj")
         {
             Debug.Log("This worked");
             StartLevel();
         }
     }
-
 
     public void KinectSelect()
     {
